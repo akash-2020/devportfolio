0d8b7d44270b4973834b4b19da75ff68f54f7526
akash-2020 <aakashbrahmbhatt31@gmail.com>
2020-07-06 14:55:27 +0530

    Initial Commit

---
+++ cssgrid.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CSS Grid</title>
+    <link rel="stylesheet" href="images/style.css">
+</head>
+<body>
+    <div class="container">
+        <div id="header">csF</div>
+        <div id="nav">aery</div>
+        <div id="main">aer zdfgtfg</div>
+        <div id="sidebar">vaerg</div>
+        <div id="footer">faerf</div>
+    </div>
+</body>
+</html>
