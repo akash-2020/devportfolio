7df6742fc52aec48648a859075ac4ccb9006faa5
akash-2020 <aakashbrahmbhatt31@gmail.com>
2020-12-16 11:38:59 +0530

    Version Link Added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 function openProject() {
-  window.open("https://github.com/akash-2020/devportfolio", "_blank");
+  window.open(
+    "https://github.com/akash-2020/devportfolio/commit/b036ba3f4fd26d537f9a1635f4dd4706dfa9c9b8",
+    "_blank"
+  );
 }
