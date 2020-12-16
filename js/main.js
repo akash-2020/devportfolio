87ce683c7264a0631c34dcc0b36bba8e004170bd
akash-2020 <aakashbrahmbhatt31@gmail.com>
2020-12-16 11:51:43 +0530

    Changes to gh-pages

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
 function openProject() {
-  window.open(
-    "https://github.com/akash-2020/devportfolio/commit/b036ba3f4fd26d537f9a1635f4dd4706dfa9c9b8",
-    "_blank"
-  );
+  window.open("https://github.com/akash-2020/devportfolio/", "_blank");
 }
