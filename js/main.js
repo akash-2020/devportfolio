d0797b4111eb39ccb6af56f3d9657edd6e054e10
akash-2020 <aakashbrahmbhatt31@gmail.com>
2020-12-16 10:53:37 +0530

    Changed

--- js/main.js
+++ js/main.js
@@ -1,24 +1,3 @@
 function openProject() {
   window.open("https://github.com/akash-2020/devportfolio", "_blank");
 }
-
-$(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
-    loop: true,
-    dots: true,
-    margin: 10,
-    responsiveClass: true,
-    autoplay: true,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      600: {
-        items: 3,
-      },
-      1000: {
-        items: 5,
-      },
-    },
-  });
-});
